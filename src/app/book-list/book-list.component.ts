c56894de09a6af21e7271b1c7f4fc29208b49147
musta55 <bsse1114@iit.du.ac.bd>
2022-03-30 20:03:02 +0600

    update  and delete done

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -11,15 +11,17 @@ import { BookService } from '../book.service';
 export class BookListComponent implements OnInit {
 
   constructor(private bookService: BookService, private router : Router) { }
+  
 books : Book[]=[];
   ngOnInit(): void {
+    //service theke view te pacche
     this.books = this.bookService.getBooks();
-
   }
 
-  deleteBook(book: Book){
-    console.log(book)
-    this.books=this.bookService.deleteBook(book);
+  deleteBook(bookTobeDeleted: Book){
+    console.log(bookTobeDeleted)
+    //then bookservice book delete kore baki list gula pathabe
+    this.books=this.bookService.deleteBook(bookTobeDeleted);
   }
 
   updateBook(book: Book){
--- src/app/book.ts
+++ src/app/book.ts
@@ -1,8 +1,9 @@
 export class Book
 {
-
     id: number=0;
     name: string ="";
+    //author: string="";
     year: number=2022;
     availability: boolean = true;
+   
 }
